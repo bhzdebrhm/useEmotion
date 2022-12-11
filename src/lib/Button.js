b95b565d8728b0c12b6c70d4946ed30cad5b689c
bhzdebrhm <bhzd.ebrhm@gmail.com>
2022-12-11 18:28:26 +0330

    save

--- src/lib/Button.js
+++ src/lib/Button.js
@@ -1,10 +1,3 @@
 const Button = (props) => {
-    const btnStyle = {
-        color: "#fff",
-        backgroundColor: "#0069d9",
-        fontSize: "14px"
-    }
-    return (
-        <button style={btnStyle}>{props}</button>
-    )
+   return "test"
 }
