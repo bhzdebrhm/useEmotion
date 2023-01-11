0bc59bd075c357adc6572b33641b7e6b6d2af9f5
bhzdebrhm <bhzd.ebrhm@gmail.com>
2023-01-11 10:20:39 +0330

    fix immer import

--- src/emotion/useEmotion.ts
+++ src/emotion/useEmotion.ts
@@ -1,3 +1,4 @@
+import { Updater, useImmer } from '@bhzdebrhm/react-hooks';
 import { create, NanoRenderer } from 'nano-css';
 import { addon as addonCSSOM, CSSOMAddon } from 'nano-css/addon/cssom';
 import { addon as addonVCSSOM, VCSSOMAddon } from 'nano-css/addon/vcssom';
@@ -6,7 +7,6 @@ import React, { DependencyList } from 'react';
 import { useTheme } from '..';
 import { useMemo } from 'react';
 import { computeCss } from '../utils';
-import { Updater, useImmer } from '@bhzdebrhm/react-hooks/dist/useImmer';
 
 type Nano = NanoRenderer & CSSOMAddon & VCSSOMAddon;
 const nano = create() as Nano;
